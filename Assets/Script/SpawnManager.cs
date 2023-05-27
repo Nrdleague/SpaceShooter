cc4a44aaee59ebce28f5824b2946503b8036c1ba
Nrdleague <51178386+Nrdleague@users.noreply.github.com>
2023-05-27 14:09:11 -0400

    Fin balanced spawning system

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,22 +10,30 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _enemyPrefab;
+    private GameObject[] _enemyPrefab;
     [SerializeField ]
     private GameObject _enemyContainer;
     [SerializeField]
     private GameObject [] powerUps;
+    int _enemyInWave;
 
     float _waitTimeEnemy = 5.0f;
     float _waitTimeWaves = 7.0f;
     int _maxEnemies = 1;
     int _enemiesSpawned = 0;
     private GameManager _gameManager;
+ 
 
     int _xPositionLimit;
     int _yPositionLimit;
     float _randomZangle;
 
+    //Balance Spawning
+    int _weightedTotal;
+    int _weight;
+    int _powerupToSpawn;
+    int _maxSpawnEnemyNum;
+    int _spawnEnemyNumber;
 
 
     private bool _stopSpawning = false;
@@ -37,19 +48,116 @@ public class SpawnManager : MonoBehaviour
         }
 
         Debug.Log("SpawnManagerS::Start() Called. _stopSpawning=" + _stopSpawning.ToString());
+    
+       
     }
 
+    
+   
 
     public void StartSpawning(int _waveID)
     {
         _stopSpawning = false;
         GetWaveInfo(_waveID);
-        //StartCoroutine(spawnEnemyRoutine(_waitTimeEnemy));
+       
         StartCoroutine(SpawnEnemyRoutine(_waitTimeEnemy));
         StartCoroutine(SpawnPowerUpRoutine());
         
     }
+
+    void powerUpSelect()
+    {
+        _weightedTotal = 0;
+        int[] powerupTable =
+        {
+
+            100, //homig missile
+            80, //ammo
+            60, // Triple shot
+            40, //speed
+            20, //shield
+            5 // Health
+
+        };
+        int[] powerupAward =
+        {
+            3, // Ammo
+            5, // Homing missile
+            4, // Health
+            2, //speed
+            1, //shield
+            0, //triple shot
+
+        };
+
+        foreach(var item in powerupTable)
+        {
+            _weightedTotal += item;
+        }
+
+        var randomNumber = Random.Range(0, _weightedTotal);
+        var i = 0;
+        
+        foreach(var item in powerupTable)
+        {
+            if(randomNumber <= _weight)
+            {
+                _powerupToSpawn = powerupAward[i];
+                return;
+            }
+            else
+            {
+                i++;
+                randomNumber -= _weight;
+            }
+        }
+    } 
+
+    void EnemySelect()
+    {
+        _weightedTotal = 0;
+
+        var _maxSpawnEnemyNum = _enemyInWave;
+        if(_maxSpawnEnemyNum > _enemyPrefab.Length)
+        {
+            _maxSpawnEnemyNum = _enemyPrefab.Length;
+        }
+
+
+        int[] enemyTable =
+        {
+            40, //0 main enemy
+            20, // 1 orb enemy
+            
+        };
+        int[] enemyID =
+        {
+            0,
+            1
+        };
+        for(int i = 0; i < _maxSpawnEnemyNum; i++)
+        {
+            _weightedTotal += enemyTable[i];
+        }
+
+        var randomNumber = Random.Range(0, _weightedTotal);
+        var x = 0;
+        foreach (var weight in enemyTable)
+        {
+            if(randomNumber <= weight)
+            {
+                _spawnEnemyNumber = enemyID[x];
+                return;
+            }
+            else
+            {
+                x++;
+                randomNumber -= weight;
+            }
+        }
+    }
     
+
    
     public void StopSpawning()
     {
@@ -100,33 +208,7 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    //IEnumerator spawnEnemyRoutine(float _waitTimeEnemy)
-    //{
-    //    Debug.Log("SpawnManager::spawnEnemyRoutine() Called");
-
-    //    while (!_stopSpawning)
-     //   {
-      //     for (int i = 0; i < _maxEnemies; i++)
-     //       {
-          //      yield return new WaitForSeconds(_waitTimeEnemy);
-
-          //      if (!_stopSpawning)
-          //      {
-                    //Instantiate Enemy Prefab
-          //          float _randomX = Random.Range(-_xPositionLimit, _xPositionLimit);
-           //         _randomZangle = Random.Range(-45f, 45f);
-           //         Vector3 spawnPosition = new Vector3(_randomX, _yPositionLimit, 0);
-           //         GameObject newEnemy = Instantiate(_enemyPrefab, spawnPosition, Quaternion.Euler(0, 0, _randomZangle));
-              //      _enemiesSpawned++;
-              //      newEnemy.transform.parent = _enemyContainer.transform;
-
-           //     }
-           // }
-
-       //     yield return new WaitForSeconds(_waitTimeWaves);
-       // }
-
-    //}
+    
 
 
 
@@ -136,11 +218,13 @@ public class SpawnManager : MonoBehaviour
 
         yield return new WaitForSeconds(4.0f);
 
+        
+
 
         while(_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(8.0f, -9.0f), 7, 0);
-            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            GameObject newEnemy = Instantiate(_enemyPrefab[Random.Range(0, _spawnEnemyNumber)], posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             _enemiesSpawned++;
             newEnemy.transform.parent = _enemyContainer.transform;
@@ -161,11 +245,12 @@ public class SpawnManager : MonoBehaviour
             Instantiate(powerUps[randompowerup], posToSpawn, Quaternion.identity);
             yield return new WaitForSeconds(Random.Range(3, 7.0f));
             
-
+            
         }
 
     }
 
+    
   
   
 
